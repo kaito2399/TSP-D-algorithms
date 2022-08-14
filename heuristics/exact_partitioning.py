39699b15477c83ca909f96e5ed092a41d3a30b59
kaito2399 <kaito.miura.23@gmail.com>
2022-08-14 21:47:06 +0900

    greedy partitioning

---
+++ heuristics/exact_partitioning.py
@@ -0,0 +1,8 @@
+import numpy
+import sys
+sys.path.append('c:\\東工大\\塩浦研\\卒論\\プログラム\\other_functions')
+sys.path.append("../")
+from generating_functions import *
+from route_drawing import *
+from basic_functions import *
+import itertools
---
+++ heuristics/greedy_partitioning.py
@@ -0,0 +1,138 @@
+import numpy
+import sys
+sys.path.append('c:\\東工大\\塩浦研\\卒論\\プログラム\\other_functions')
+sys.path.append("../")
+from generating_functions import *
+from route_drawing import *
+from basic_functions import *
+from solving_TSP import *
+import time
+
+def greedy_partitioning(V,tsp,depot,alpha):#depotはindex指定ではない
+    label={}
+    operation={}
+    flyop={}
+    driveop={}
+    tsptotal=0
+    for i in tsp:
+        label[i]='simple'
+        tsptotal=tsptotal+t_cost(V[tsp[i]],V[tsp[(i+1)%len(tsp)]])
+    n=len(label)
+    tspdtotal=tsptotal
+    while 'simple' in label.values():
+        ms=[]
+        for i in range(n):
+            if label[tsp[i]]!='simple' or tsp[i]==depot:
+                ms.append(-float('inf'))
+            else:
+                mfsi=t_cost(V[tsp[i-1]],V[tsp[i]])+t_cost(V[tsp[i]],V[tsp[(i+1)%n]])-max(d_cost(V[tsp[i-1]],V[tsp[i]],alpha)+d_cost(V[tsp[i]],V[tsp[(i+1)%n]],alpha),t_cost(V[tsp[i-1]],V[tsp[(i+1)%n]]))
+                ms.append(mfsi)
+        for i in range(n):
+            if label[tsp[i-1]]=='combined' and tsp[(i-1)]!=depot:
+                if label[tsp[i]]=='simple':
+                    j=i-2
+                    while label[tsp[j]]!='combined':
+                        if label[tsp[j]]=='drone':
+                            dronenode=j
+                        if label[tsp[(j-1)]]=='combined':
+                            pls=t_cost(V[tsp[i-1]],V[tsp[i]])+operation[(tsp[dronenode],tsp[j-1],tsp[i-1])]-max(d_cost(V[tsp[j-1]],V[tsp[dronenode]],alpha)+d_cost(V[tsp[dronenode]],V[tsp[i]],alpha),driveop[(tsp[dronenode],tsp[j-1],tsp[i-1])]+t_cost(V[tsp[i-1]],V[tsp[i]]))
+                            ms.append(pls)
+                            break
+                        j=j-1  
+                else: ms.append(-float('inf'))
+            else: ms.append(-float('inf'))
+        for i in range(n):
+            if label[tsp[(i+1)%n]]=='combined' and tsp[(i+1)%n]!=depot:
+                if label[tsp[i]]=='simple':
+                    j=(i+2)%n
+                    while label[tsp[j]]!='combined':
+                        if label[tsp[j]]=='drone':
+                            dronenode=j
+                        if label[tsp[(j+1)%n]]=='combined':
+                            prs=t_cost(V[tsp[i]],V[tsp[(i+1)%n]])+operation[(tsp[dronenode],tsp[(i+1)%n],tsp[(j+1)%n])]-max(d_cost(V[tsp[i]],V[tsp[dronenode]],alpha)+d_cost(V[tsp[dronenode]],V[tsp[(j+1)%n]],alpha),driveop[(tsp[dronenode],tsp[(i+1)%n],tsp[(j+1)%n])]+t_cost(V[tsp[i]],V[tsp[(i+1)%n]]))
+                            ms.append(prs)
+                            break
+                        j=(j+1)%n
+                else: ms.append(-float('inf'))
+            else: ms.append(-float('inf'))
+        i = numpy.argmax(ms)
+        if ms[i]>0:
+            tspdtotal=tspdtotal-ms[i]
+            if i//n==0:#makeflysaving
+                label[tsp[i]]='drone'
+                label[tsp[i-1]]='combined'
+                label[tsp[(i+1)%n]]='combined'
+                flyop[(tsp[i],tsp[i-1],tsp[(i+1)%n])]=d_cost(V[tsp[i-1]],V[tsp[i]],alpha)+d_cost(V[tsp[i]],V[tsp[(i+1)%n]],alpha)
+                driveop[(tsp[i],tsp[i-1],tsp[(i+1)%n])]=t_cost(V[tsp[i-1]],V[tsp[(i+1)%n]])
+                operation[(tsp[i],tsp[i-1],tsp[(i+1)%n])]=max(flyop[(tsp[i],tsp[i-1],tsp[(i+1)%n])],driveop[(tsp[i],tsp[i-1],tsp[(i+1)%n])])
+            elif i//n==1:#pushleftsaving
+                label[tsp[i%n]]='combined'
+                label[tsp[(i-1)%n]]='truck'
+                j=(i-2)%n
+                while label[tsp[j]]!='combined':
+                    if label[tsp[j]]=='drone':
+                        dronenode=j
+                    if label[tsp[j-1]]=='combined':
+                        flyop[(tsp[dronenode],tsp[j-1],tsp[i%n])]=d_cost(V[tsp[j-1]],V[tsp[dronenode]],alpha)+d_cost(V[tsp[dronenode]],V[tsp[i%n]],alpha)
+                        driveop[(tsp[dronenode],tsp[j-1],tsp[i%n])]=driveop[(tsp[dronenode],tsp[j-1],tsp[(i-1)%n])]+t_cost(V[tsp[(i-1)%n]],V[tsp[i%n]])
+                        operation[(tsp[dronenode],tsp[j-1],tsp[i%n])]=max(flyop[(tsp[dronenode],tsp[j-1],tsp[i%n])],driveop[(tsp[dronenode],tsp[j-1],tsp[i%n])])
+                        break
+                    j=j-1
+            elif i//n==2:#pushrightsaving
+                label[tsp[i%n]]='combined'
+                label[tsp[(i+1)%n]]='truck'
+                j=(i+2)%n
+                while label[tsp[j]]!='combined':
+                    if label[tsp[j]]=='drone':
+                        dronenode=j
+                    if label[tsp[(j+1)%n]]=='combined':
+                        flyop[(tsp[dronenode],tsp[i%n],tsp[(j+1)%n])]=d_cost(V[tsp[i%n]],V[tsp[dronenode]],alpha)+d_cost(V[tsp[dronenode]],V[tsp[(j+1)%n]],alpha)
+                        driveop[(tsp[dronenode],tsp[i%n],tsp[(j+1)%n])]=driveop[(tsp[dronenode],tsp[(i+1)%n],tsp[(j+1)%n])]+t_cost(V[tsp[i%n]],V[tsp[(i+1)%n]])
+                        operation[(tsp[dronenode],tsp[i%n],tsp[(j+1)%n])]=max(flyop[(tsp[dronenode],tsp[i%n],tsp[(j+1)%n])],driveop[(tsp[dronenode],tsp[i%n],tsp[(j+1)%n])])
+                        break
+                    j=(j+1)%n
+        else:
+            for l in label:
+                if label[l]=='simple':
+                    label[l]='combined'
+
+    
+    dset=set()
+    for i in label:
+        if label[i]=='drone':
+            dset.add(i)
+    return tspdtotal,label,dset
+
+# initial route is calculated by using mst 2-approximation algorithm for TSP.    
+def MST_greedy_partitioning(V,depot,alpha):
+    tsp=two_approximation_for_TSP(V)
+    return greedy_partitioning(V,tsp,depot,alpha)
+
+# initial route is calculated by using DP for TSP.
+def DP_greedy_partitioning(V,depot,alpha):
+    tsp=DP_for_TSP(V)
+    return greedy_partitioning(V,tsp,depot,alpha)
+
+
+def main():
+  # you can change the size of the problems here
+  n = 10
+  # you can change the character of the testcases here
+  V = testcase_donuts_center(n)
+  # you can change the spped rate between truck and drone here
+  alpha = 2
+  start = time.time()
+  total_cost,label,drone_nodes = MST_greedy_partitioning(V,0,alpha)
+  end = time.time()
+  print(f"running time of MST_greedy_partitioning : {round(end-start,4)} sec")
+  print(f"total cost (time) to deliver all of the customers : {round(total_cost,4)}")
+  print("------------------------------------------------------------------------------------------------------------------------------------------------")
+  start = time.time()
+  total_cost,label,drone_nodes = DP_greedy_partitioning(V,0,alpha)
+  end = time.time()
+  print(f"running time of DP_greedy_partitioning : {round(end-start,4)} sec")
+  print(f"total cost (time) to deliver all of the customers : {round(total_cost,4)}")
+#   drawing_routes(V,route,0,drone_nodes)
+
+if __name__ == '__main__':
+  main()
